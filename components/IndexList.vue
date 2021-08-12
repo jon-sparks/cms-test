81154d769f163dc2bb1183ca8c4152547ef447af
Jon Sparks <jon.sparks@sotic.net>
2021-08-12 13:24:01 +0100

    style change

--- components/IndexList.vue
+++ components/IndexList.vue
@@ -8,14 +8,15 @@
     >
       <div class="index-item__image">
         <img :src="item['featured-image']" alt="">
+        <h3>{{ item.title }}</h3>
       </div>
-      <div class="index-item__content">
+      <!-- <div class="index-item__content">
         <h3>{{ item.title }}</h3>
         <p>{{ item.description }}</p>
         <button>
           VIEW
         </button>
-      </div>
+      </div> -->
     </nuxt-link>
   </section>
 </template>
@@ -26,11 +27,11 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .index-list {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  column-gap: 1rem;
+  column-gap: 3rem;
   row-gap: 3rem;
 }
 
@@ -40,16 +41,36 @@ export default {
   flex-direction: column;
   text-decoration: none;
   color: var(--tertiary);
-  overflow: hidden;
 }
 
 .index-item h3 {
   font-size: 30px;
 }
 
+.index-item__image {
+  padding: 2rem;
+  position: relative;
+  background: #eb9e00;
+  border-radius: 25px;
+  width: 369px;
+  height: 369px;
+}
+
 .index-item__image img {
-  display: block;
-  width: 100%;
+  position: absolute;
+  top: -50px;
+  right: -40px;
+  z-index: 1;
+}
+
+.index-item__image h3 {
+  position: absolute;
+  bottom: 0;
+  left: 29px;
+  color: #252525;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 72px;
 }
 
 .index-item__content {
