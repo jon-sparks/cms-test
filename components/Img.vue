e66ac78c6182a9554c7ee6575100a112ff6609aa
Jon Sparks <jon.sparks@sotic.net>
2021-08-13 09:16:17 +0100

    products index

---
+++ components/Img.vue
@@ -0,0 +1,34 @@
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+  >
+</template>
+
+<script>
+export default {
+  props: [`source`, `alt`, `width`, `height`],
+  computed: {
+    src () {
+      if (this.source) {
+        return this.$cloudinary.image.url(`/${this.getId(this.source)}`, {
+          width: this.width ? this.width : null,
+          height: this.height ? this.height : null,
+          crop: 'scale'
+        })
+      }
+      return false
+    },
+  },
+  methods: {
+    getId (path) {
+      const index1 = path.lastIndexOf(`/`, path.lastIndexOf(`/`)-1)
+      const index2 = path.lastIndexOf(`.`)
+      return path.substring(index1, index2)
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- components/IndexList.vue
+++ components/IndexList.vue
@@ -8,16 +8,13 @@
     >
       <AspectRatio data-aspect="1:1">
         <div class="index-item__image">
-          <img :src="item['featured-image']" alt="">
+          <Img
+            :source="item['featured-image']"
+            :alt="`alt`"
+            :width="330"
+          />
           <h3>{{ item.title }}</h3>
         </div>
-        <!-- <div class="index-item__content">
-          <h3>{{ item.title }}</h3>
-          <p>{{ item.description }}</p>
-          <button>
-            VIEW
-          </button>
-        </div> -->
       </AspectRatio>
     </nuxt-link>
   </section>
@@ -35,6 +32,7 @@ export default {
   grid-template-columns: 1fr 1fr 1fr 1fr;
   column-gap: 6rem;
   row-gap: 6rem;
+  margin-top: 10rem;
 }
 
 .index-item {
@@ -43,52 +41,52 @@ export default {
   flex-direction: column;
   text-decoration: none;
   color: var(--tertiary);
-}
 
-.index-item h3 {
-  font-size: 24px;
-}
+  h3 {
+    font-size: 24px;
+  }
 
-.index-item__image {
-  padding: 2rem;
-  position: relative;
-  background: #eb9e00;
-  border-radius: 25px;
-  width: 100%;
-  height: 100%;
-}
+  &__image {
+    padding: 2rem;
+    position: relative;
+    background: #eb9e00;
+    border-radius: 25px;
+    width: 100%;
+    height: 100%;
 
-.index-item__image img {
-  position: absolute;
-  top: -50px;
-  right: -40px;
-  width: 100%;
-  z-index: 1;
-}
+    img {
+      position: absolute;
+      top: -50px;
+      right: -40px;
+      width: 100%;
+      z-index: 1;
+    }
 
-.index-item__image h3 {
-  position: absolute;
-  bottom: 10px;
-  left: 29px;
-  color: #252525;
-  text-transform: uppercase;
-  font-weight: bold;
-  font-size: 50px;
-}
+    h3 {
+      position: absolute;
+      bottom: 10px;
+      left: 29px;
+      color: #252525;
+      text-transform: uppercase;
+      font-weight: bold;
+      font-size: 50px;
+    }
+  }
 
-.index-item__content {
-  padding: 2rem;
-  background: white;
-}
+  &__content {
+    padding: 2rem;
+    background: white;
 
-.index-item__content button {
-  font-family: 'Titillium Web', sans-serif;;
-  font-size: 22px;
-  margin-top: 2rem;
-  padding: .25rem 2rem;
-  border: none;
-  background: var(--tertiary);
-  color: var(--white);
-  pointer-events: none;
+    button {
+      font-family: 'Titillium Web', sans-serif;;
+      font-size: 22px;
+      margin-top: 2rem;
+      padding: .25rem 2rem;
+      border: none;
+      background: var(--tertiary);
+      color: var(--white);
+      pointer-events: none;
+    }
+  }
 }
 </style>
