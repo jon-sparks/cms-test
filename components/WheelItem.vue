27b9dbec6913097ebc9eabf4923f5e4ec9cfc226
Jon Sparks <jon.sparks@sotic.net>
2021-08-21 17:46:02 +0100

    whee item

--- components/WheelItem.vue
+++ components/WheelItem.vue
@@ -3,16 +3,17 @@
     :to="link"
     class="index-item"
   >
-    <AspectRatio ratio="1:1">
-      <div class="index-item__image">
-        <Img
-          :source="image"
-          :alt="`alt`"
-          :width="330"
-        />
-        <!-- <h3>{{ item.title }}</h3> -->
-      </div>
-    </AspectRatio>
+    <client-only>
+      <AspectRatio ratio="1:1">
+        <div class="index-item__image">
+          <Img
+            :source="image"
+            :alt="`alt`"
+            :width="330"
+          />
+        </div>
+      </AspectRatio>
+    </client-only>
   </nuxt-link>
 </template>
 
